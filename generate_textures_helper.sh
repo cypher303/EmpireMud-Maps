dccdc7b2ca6933a0ecee524c2f5c5705911ae4ba
Scott <scott@Scotts-MacBook-Pro.local>
2025-12-06 16:36:35 -0600

    helper script update, probably redundant...

--- generate_textures_helper.sh
+++ generate_textures_helper.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 cd /Users/scott/WebstormProjects/Empire-Maps-Multiple-Projects/Empire-Maps || exit 1
-npm run generate:textures -- --force
+npm run generate:textures -- --force --purge
+
+# node --import tsx tools/generate-textures.ts --force --purge
